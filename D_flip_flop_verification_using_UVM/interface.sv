59ea37a24041fe35b1377d42823dffeed1f54fdd
mano-3-1-2002 <manovijay383@gmail.com>
2025-01-02 20:46:25 +0530

    Update interface.sv

--- D_flip_flop_verification_using_UVM/interface.sv
+++ D_flip_flop_verification_using_UVM/interface.sv
@@ -1,1 +1,10 @@
-
+//interface between DUT and Testbench
+interface dff_intf(input logic clk);
+   //interface has all dut signals
+  //Declare with logic type
+  //clock is generating and comming from (testbench.sv) top module so it is input for interface
+  //logic clk;
+  logic d;
+  logic rst;
+  logic q;
+endinterface
