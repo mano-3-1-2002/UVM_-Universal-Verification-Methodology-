1b5467ea30820e5d5bbba27c641eb9d28dd259ec
mano-3-1-2002 <manovijay383@gmail.com>
2025-01-02 20:46:58 +0530

    Update seq_item.sv

--- D_flip_flop_verification_using_UVM/seq_item.sv
+++ D_flip_flop_verification_using_UVM/seq_item.sv
@@ -1,1 +1,15 @@
 
+class dff_seq_item extends uvm_sequence_item;
+  `uvm_object_utils(dff_seq_item)
+  // reg class to uvm factory
+  rand logic d;
+  logic rst;
+  //logic clk; no need because we gave in interface
+  logic q;
+  
+  function new(string name = "dff_seq_item");
+    super.new(name);
+    'uvm_info("sequence item class","constructor",UVM_MEDIUM)
+  endfunction
+  //no phases
+endclass
