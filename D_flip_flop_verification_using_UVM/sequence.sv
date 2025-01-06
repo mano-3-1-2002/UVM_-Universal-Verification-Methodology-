26785e6f1de50cde1a7162a079f9ec9e15793272
mano-3-1-2002 <manovijay383@gmail.com>
2025-01-06 21:08:52 +0530

    Update sequence.sv

--- D_flip_flop_verification_using_UVM/sequence.sv
+++ D_flip_flop_verification_using_UVM/sequence.sv
@@ -1,19 +1,27 @@
 class dff_sequence extends uvm_sequence#(dff_seq_item);
-  `uvm_object_utils(dff_sequence)
+  `uvm_object_utils(dff_sequence) // reg class to uvm factory
+  
   dff_seq_item tx;
-  function new(string name = "dff_sequence");
+  
+  //standard constructor
+  function new(string name ="dff_sequence");
     super.new(name);
-    `uvm_info("sequence class","constructor",UVM_MEDIUM)
+    `uvm_info("sequence Class", "constructor", UVM_MEDIUM)
   endfunction
-
+  
+  //no phase
+  
   task body();
     repeat(5) begin
-      tx = dff_seq_item::type_id::create("tx");
       
-      wait_for_grant();
-      tx.randomize();
-      send_request(tx);
-      wait_for_item_done();
+    tx = dff_seq_item ::type_id::create("tx");
+    
+    wait_for_grant();
+    tx.randomize();// with (rst==1);
+    send_request(tx);
+    wait_for_item_done();
+    
     end
   endtask
+    
 endclass
